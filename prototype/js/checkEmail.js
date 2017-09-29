b49c771857a80019a4367d7a18915298f9f3b4fe
jsmith <jsmith@cs.nmt.edu>
2017-09-29 16:32:05 -0600

    fixed next button, js, styling

--- prototype/js/checkEmail.js
+++ prototype/js/checkEmail.js
@@ -4,16 +4,18 @@ function checkEmail() {
     var split = email.split("@");
 
     var message = document.getElementById("message");
+    var button = document.getElementById("nextButton")
 
     if (split.length == 2 && split[1] == "student.nmt.edu"){
-      document.getElementById("nextButton").disabled = false;
+      button.disabled = false;
+      button.style = "background-color: #373737"
       message.innerHTML = "Email address valid";
       message.style = "color: green;";
     }
     else {
-      document.getElementById("nextButton").disabled = true;
+      button.disabled = true;
+      button.style = "background-color: #CCC"
       message.innerHTML = "Email address invalid. Must end in @student.nmt.edu";
       message.style = "color: red;";
     }
-
 }
