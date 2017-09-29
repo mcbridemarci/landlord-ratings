d3fd2838cc1e3a9c13cead391538fdb5b3b6af9a
jsmith <jsmith@cs.nmt.edu>
2017-09-29 16:03:54 -0600

    checking emails and js

--- prototype/js/checkEmail.js
+++ prototype/js/checkEmail.js
@@ -6,10 +6,12 @@ function checkEmail() {
     var message = document.getElementById("message");
 
     if (split.length == 2 && split[1] == "student.nmt.edu"){
+      document.getElementById("nextButton").disabled = false;
       message.innerHTML = "Email address valid";
       message.style = "color: green;";
     }
     else {
+      document.getElementById("nextButton").disabled = true;
       message.innerHTML = "Email address invalid. Must end in @student.nmt.edu";
       message.style = "color: red;";
     }
