6a1d9ebf2b4a70776107f0b27d67ac452cb528ff
marci <marci@cs.nmt.edu>
2017-11-26 21:45:03 -0700

    working on the map

--- finalSystem/web/js/map.js
+++ finalSystem/web/js/map.js
@@ -9,9 +9,29 @@ function initMap() {
   setMarkers(map);
 }
 /* Get locations that have reviews */
+$.getJSON('locations.json', function (json) {
+    var array = [];
+    for (var key in json) {
+        if (json.hasOwnProperty(key)) {
+            var item = json[key];
+            array.push({ addr: item.address1, lat: item.latitude, long: item.longitude, zindex: 4});            
+        }
+    }
+    console.log(array);
+});
+
+/* expected jason file format 
+{
+    "key":{
+        "address1":"1234 Main Street",
+        "address2":"1",
+        "lat":34.067599,
+        "long":-106.902407
+    },
+*/
 var reviewLocations = [
   ['1234 Main Street', 34.067599, -106.902407, 4]
-];
+]; 
 
 /* create and ddd markers to the map */
 function setMarkers(map) {
@@ -24,14 +44,14 @@ function setMarkers(map) {
       position: {lat: review[1], lng: review[2]},
       map: map,
       draggable: false,
-      label: "R",
+      /*label: "R",*/
       title: review[0],
       zIndex: review[3]
     });
     /* create place details */
     google.maps.event.addListener(marker, 'click', (function (marker, i) {
           return function () {
-              infowindow.setContent('<h5>'+reviewLocations[i][0]+'</h5><h6><a href="review_preview.html">Click here to see the reviews</a></h6>');
+              infowindow.setContent('<h5>'+reviewLocations[i][0]+'</h5><h6><a href="review_preview.jsp">Click here to see the reviews for this home</a></h6>');
               infowindow.open(map, marker);
           }
       })(marker, i)); 
