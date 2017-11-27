bc52abc8442b016d9d77fa57b785fdd22c6fa4b4
marci <marci@cs.nmt.edu>
2017-11-27 00:09:15 -0700

    working on creaing json from server data

--- finalSystem/src/java/MapServlet.java
+++ finalSystem/src/java/MapServlet.java
@@ -1,20 +1,18 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-import java.io.IOException;
-import java.io.PrintWriter;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.*;
+import static java.lang.Integer.parseInt;
+import javax.servlet.*;
+import java.sql.*;
+import javax.servlet.http.HttpSession;
+import java.io.FileWriter;
+import java.io.IOException;
+ 
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
 
-/**
- *
- * @author marci_home
- */
 public class MapServlet extends HttpServlet {
 
     /**
@@ -28,19 +26,45 @@ public class MapServlet extends HttpServlet {
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet MapServlet</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet MapServlet at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
-        }
+            try{
+                String driver = "org.mariadb.jdbc.Driver";
+                Class.forName(driver);
+                String dbURL = "jdbc:mariadb://localhost:3306/apollo_4_project";
+                Connection connection = DriverManager.getConnection(dbURL, "apollo.4", "zozoZOZO");
+                Statement statement = connection.createStatement();
+                
+                System.out.println("connected");
+                
+                String sql = (
+                    "SELECT 'postNumber', `latitude`, `longitude`, `address1`,`address2`" + "FROM `apollo_4_project`.`address`"
+                );
+                //https://stackoverflow.com/questions/12041354/java-getting-data-from-mysql-database
+                //https://alvinalexander.com/java/java-mysql-select-query-example
+                System.out.println(sql);
+ 
+		JSONArray company = new JSONArray();
+		company.add("latitude: eBay");
+		company.add("longitude: Paypal");
+		company.add("address1: Google");
+                company.add("address2: Google");
+		obj.put(postNumber,company);
+ 
+		// try-with-resources statement based on post comment below :)
+		try (FileWriter file = new FileWriter("/Users/<username>/Documents/file1.txt")) {
+			file.write(obj.toJSONString());
+			System.out.println("Successfully Copied JSON Object to File...");
+			System.out.println("\nJSON Object: " + obj);
+		}
+                
+                
+                query.close();
+                statement.close();
+                connection.close();
+            } catch (ClassNotFoundException ex) {
+                System.err.println("Error with connection: " + ex);
+            } catch (SQLException ex) {
+                System.err.println("Error loading driver: " + ex);
+            }
     }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
